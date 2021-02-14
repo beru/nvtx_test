4037209c1bc1eb1e3fa98195025e7368db8b8380
beru <berupon@gmail.com>
2021-02-15 04:45:11 +0900

    update chart plotting script

--- chart3.py
+++ chart3.py
@@ -3,8 +3,12 @@
 import sys
 import os
 import sqlite3
+import matplotlib as mpl
 from matplotlib import pyplot as plt
+from cycler import cycler
+
 import numpy as np
+from pprint import pprint
 
 def main():
     if len(sys.argv) < 2:
@@ -39,47 +43,57 @@ def main():
 
     labels = []
     durations = []
-    i = 0
-    for j in range(num_of_dbs):
-        durations.append([])
     for row in c.execute(sql):
         #print(row)
         labels.append(row[0])
-        lst = []
-        for j in range(num_of_dbs):
-            durations[j].append(row[1+j])
-        i += 1
+        durations.append(row[1:])
     conn.close()
     x = np.arange(len(labels))
-    width = 1.5 / (num_of_dbs * len(labels))
+    height = (len(labels) - 1) / (num_of_dbs * len(labels))
+
+    n = num_of_dbs
+    plt.rcParams['axes.prop_cycle']  = cycler(color=[plt.get_cmap('hot')(1. * i/n) for i in range(n)])
     fig, ax = plt.subplots()
 
     def autolabel(rects):
         """Attach a text label above each bar in *rects*, displaying its height."""
         for rect in rects:
             width = rect.get_width()
+            if width == 0:
+                continue
             ax.annotate('{:.1f}'.format(width/1e9),
                         xy=(width, rect.get_y() + rect.get_height() / 2),
                         xytext=(3, 0),
                         textcoords="offset points",
-                        ha='left', va='center')
+                        ha='left', va='center',
+                        fontsize=12 - len(rects))
 
+    durations = list(map(list, zip(*durations)))
+    # pprint(durations)
 
+    basenames_without_ext = []
+    rects = []
+    ind = np.arange(len(labels))
     for i in range(num_of_dbs):
-        autolabel(ax.barh(-(num_of_dbs*width)/2 + width/2 + x + width*i, durations[i], width * 0.95, label=os.path.splitext(db_filenames[i])[0]))
+        basename_without_ext = os.path.splitext(os.path.basename(db_filenames[i]))[0]
+        basenames_without_ext.append(basename_without_ext)
+        y = ind+(-(num_of_dbs-1)/2.0+i)*height
+        r = ax.barh(y, durations[i], height -0.02, label=basename_without_ext)
+        rects.append(r)
+        autolabel(r)
     plt.yticks(x, labels)
-    ax.legend(bbox_to_anchor=(1.1, 1.05))
+    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
     # plt.yticks([1e8, 1e8 * 5, 1e9, 1e9 * 5])
     plt.xticks([])
     plt.xlabel('Time(sec)')
 
     x1,x2,y1,y2 = plt.axis()
-    plt.axis((x1,x2*1.05,y1,y2))
+    plt.axis((x1,x2*1.1,y1,y2))
     plt.gca().invert_yaxis()
 
     plt.tight_layout()
     plt.show()
-    # plt.savefig(os.path.splitext(db_filenames[0])[0] + ".svg")
+    # plt.savefig(" and ".join(basenames_without_ext) + ".svg")
 
 if __name__ == "__main__":
     main()
--- demo.py
+++ demo.py
@@ -1,12 +1,24 @@
+#!/usr/bin/python
+
 # demo.py
 
 import time
 import nvtx
 
 @nvtx.annotate(color="blue")
+def my_function_3rd():
+    time.sleep(2)
+
+@nvtx.annotate(color="blue")
+def my_function_2nd():
+    time.sleep(1)
+    my_function_3rd()
+
+@nvtx.annotate(color="blue")
 def my_function():
     for i in range(5):
         with nvtx.annotate("my_loop", color="red"):
             time.sleep(max(0, i + 2))
+    my_function_2nd()
 
 my_function()
